ce82095acafdd8a0b611e0ce717b3d7359014802
sneakyevil <sneakyevildogs@gmail.com>
2022-07-29 15:05:05 +0200

    Create xcloud-hide-stream-hud.js

---
+++ xcloud-hide-stream-hud.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Xbox Cloud Gaming - Hide Stream Hud
+// @namespace    https://steamcommunity.com/id/whx/
+// @version      1.0
+// @description  Automatically show/hide top left corner stream hud when mouse is not hovering over it.
+// @author       sneakyevil
+// @grant        none
+// @run-at       document-end
+// @include      https://www.xbox.com/*/play/launch/*/*
+// ==/UserScript==
+
+//document.getElementById('StreamHUD')
+
+var m_StreamHud = null;
+
+setInterval(function()
+{
+    m_StreamHud = document.getElementById('StreamHud');
+    if (m_StreamHud == null) return;
+
+    m_StreamHud.onmouseover = function() { m_StreamHud.style.opacity = 1.0; };
+    m_StreamHud.onmouseout = function() { m_StreamHud.style.opacity = 0.0; };
+}, 1000);
